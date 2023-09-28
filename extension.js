5bd78131b6d5760354409753debdd9e75b52b5eb
fthx <23138504+fthx@users.noreply.github.com>
2023-09-28 20:40:08 +0200

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,24 +1,33 @@
 //    Window Title Is Back
 //    GNOME Shell extension
 //    @fthx 2023
-//    Puts the title of the focused window in the GNOME Shell top panel
+//    Puts the app/icon/title of the focused window in the GNOME Shell top panel
+
 
 import Clutter from 'gi://Clutter';
 import GObject from 'gi://GObject';
+import Shell from 'gi://Shell';
 import St from 'gi://St';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 
-var WindowTitleIndicator = GObject.registerClass(
+const WindowTitleIndicator = GObject.registerClass(
 class WindowTitleIndicator extends PanelMenu.Button {
     _init() {
         super._init();
 
         this._indicator = new St.BoxLayout({style_class: 'panel-button'});
-        this._icon = new St.Icon({icon_name: 'focus-top-bar-symbolic'});
+
+        this._icon = new St.Icon({});
+        this._icon.set_fallback_gicon(null);
         this._indicator.add_child(this._icon);
+
+        this._app = new St.Label({y_align: Clutter.ActorAlign.CENTER});
+        this._indicator.add_child(this._app);
+
         this._title = new St.Label({y_align: Clutter.ActorAlign.CENTER});
         this._indicator.add_child(this._title);
 
@@ -26,13 +35,32 @@ class WindowTitleIndicator extends PanelMenu.Button {
     }
 });
 
-export default class WindowTitleIsBackExtension {
+export default class WindowTitleIsBackExtension extends Extension {
     _on_focused_window_changed() {
+        if (Main.sessionMode.isLocked) {
+            return;
+        }
+
         if (this._focused_window && this._focused_window_title_changed) {
             this._focused_window.disconnect(this._focused_window_title_changed);
         }
 
         this._focused_window = global.display.get_focus_window();
+
+        if (this._focused_window) {
+            this._focused_app = Shell.WindowTracker.get_default().get_window_app(this._focused_window);
+            if (this._focused_app) {
+                this._focused_window_button._icon.set_gicon(this._focused_app.get_icon());
+                this._focused_window_button._app.set_text(this._focused_app.get_name() + this._label_padding);
+            } else {
+                this._focused_window_button._icon.set_icon_name('applications-system-symbolic');
+                this._focused_window_button._app.set_text('');
+            }
+        } else {
+            this._focused_window_button._icon.set_gicon(null);
+            this._focused_window_button._app.set_text('');
+        }
+
         this._on_focused_window_title_changed();
 
         if (this._focused_window) {
@@ -54,9 +82,27 @@ export default class WindowTitleIsBackExtension {
         }
     }
 
+    _on_settings_changed() {
+        this._focused_window_button._icon.visible = this._settings.get_boolean('show-icon');
+        this._focused_window_button._app.visible = this._settings.get_boolean('show-app');
+        this._focused_window_button._title.visible = this._settings.get_boolean('show-title');
+
+        if (this._settings.get_boolean('show-title')) {
+            this._label_padding = '  —  ';
+        } else {
+            this._label_padding = '';
+        }
+
+        this._on_focused_window_changed();
+    }
+
     enable() {
         this._focused_window_button = new WindowTitleIndicator();
-        this._on_focused_window_changed();
+
+        this._settings = this.getSettings();
+        this._on_settings_changed();
+        this._settings_changed = this._settings.connect('changed', this._on_settings_changed.bind(this));
+
         Main.panel.addToStatusArea('focused-window-indicator', this._focused_window_button, -1, 'left');
 
         this._focused_window_changed = global.display.connect('notify::focus-window', this._on_focused_window_changed.bind(this));
@@ -81,5 +127,11 @@ export default class WindowTitleIsBackExtension {
 
         this._focused_window_button.destroy();
         this._focused_window_button = null;
+
+        if (this._settings_changed) {
+            this._settings.disconnect(this._settings_changed);
+        }
+        this._settings_changed = null;
+        this._settings = null;
     }
 }
---
+++ prefs.js
@@ -0,0 +1,44 @@
+import Gio from 'gi://Gio';
+import Adw from 'gi://Adw';
+
+import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+
+export default class WindowTitleIsBackExtensionPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        const page = new Adw.PreferencesPage({
+            title: 'Window Title Is Back extension',
+            icon_name: 'dialog-information-symbolic',
+        });
+        window.add(page);
+
+        const group = new Adw.PreferencesGroup({
+            title: 'Settings',
+        });
+        page.add(group);
+
+        const row_icon = new Adw.SwitchRow({
+            title: 'Show icon',
+        });
+        group.add(row_icon);
+
+        window._settings = this.getSettings();
+        window._settings.bind('show-icon', row_icon, 'active', Gio.SettingsBindFlags.DEFAULT);
+
+        const row_app = new Adw.SwitchRow({
+            title: 'Show window app',
+        });
+        group.add(row_app);
+
+        window._settings = this.getSettings();
+        window._settings.bind('show-app', row_app, 'active', Gio.SettingsBindFlags.DEFAULT);
+
+        const row_title = new Adw.SwitchRow({
+            title: 'Show window title',
+        });
+        group.add(row_title);
+
+        window._settings = this.getSettings();
+        window._settings.bind('show-title', row_title, 'active', Gio.SettingsBindFlags.DEFAULT);
+    }
+}
